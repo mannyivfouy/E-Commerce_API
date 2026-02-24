04e68c6b1a8b9521a409f340174e0b46cf0037e8
Mann Yivfouy <mannyivfouy@gmail.com>
2026-02-24 23:12:19 +0700

    Add Cors and Change DB.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
-
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log(`✅ MongoDB Connected`))
-  .catch((err) => console.log(`❌ MongoDB Connection Error`, err));
 
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/users", require("./routes/user.route"));
