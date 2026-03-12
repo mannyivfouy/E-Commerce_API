86eceded35b4eb74c8f0e0180aa07013b0f0585d
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-12 16:33:53 +0700

    order model

---
+++ src/models/order.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    items: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Products",
+          required: true,
+        },
+        quantity: Number,
+        price: Number,
+      },
+    ],
+    totalPrice: Number,
+    status: {
+      type: String,
+      enum: ["Pending", "Paid", "Canceled"],
+      default: "Pending",
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Orders", orderSchema);
