84a81a020fa30bc034b1ab30f83a7197b35c4986
Mann Yivfouy <mannyivfouy@gmail.com>
2026-03-26 20:23:44 +0700

    done with user login

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ["User", "Admin"], default: "User" },
-    status: { type: Boolean },
+    status: { type: Boolean, default: true },
   },
   { timestamps: true },
 );
