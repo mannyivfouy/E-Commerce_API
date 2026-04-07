f7b3ce20d9d016ffe49a1d7a9539441d1c914ae4
Mann Yivfouy <mannyivfouy@gmail.com>
2026-04-07 16:36:07 +0700

    update dashboard add status stats

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,15 +7,30 @@ const getDashBoardStats = async (req, res) => {
     const totalUsers = await Users.countDocuments();
     const totalProducts = await Products.countDocuments();
     const totalCategories = await Categories.countDocuments();
-    
+
+    const activeUsers = await Users.countDocuments({ status: true });
+    const inactiveUsers = await Users.countDocuments({ status: false });
+
+    const activeProducts = await Products.countDocuments({status : true});
+    const inactiveProducts = await Products.countDocuments({status : false});
+
+    const activeCategories = await Categories.countDocuments({status : true});
+    const inactiveCategories = await Categories.countDocuments({ status : false});
+
     res.status(200).json({
       totalUsers,
       totalProducts,
-      totalCategories
-    })
+      totalCategories,
+      activeUsers,
+      inactiveUsers,
+      activeProducts,
+      inactiveProducts,
+      activeCategories,
+      inactiveCategories
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-module.exports = {getDashBoardStats}
+module.exports = { getDashBoardStats };
